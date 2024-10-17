5a1b5cb0cfb6c917076ecb363d2483ae965b1dda
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-10-17 13:43:13 +0300

    Implement views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
