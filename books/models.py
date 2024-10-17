97ffdb9bc7e1939858639cb7f15381337d7f3e71
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-10-17 16:38:49 +0300

    register Book in admin & minor fixes

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=255, choices=COVER_CHOICES)
+    cover = models.CharField(max_length=4, choices=COVER_CHOICES)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
