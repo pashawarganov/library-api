83185fbe95f242238784b6008373473d196ddb71
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-17 15:36:13 +0300

    migrations and black

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("user.urls")),
+    path("users/", include("user.urls")),
 ]
