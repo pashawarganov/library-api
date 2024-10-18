369dde3f30d85c8e3c46ae4ca2e7fd5df031db10
olena4reunited <olenaduringintermission@gmail.com>
2024-10-18 15:33:24 +0300

    added filter for borrowings

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from django.conf import settings
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from book.models import Book
 
@@ -18,3 +21,32 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings",
     )
+
+    def __str__(self) -> str:
+        return (
+            f"Book: {self.book.title}, Author: {self.book.author}. "
+            f"Borrowed from {self.borrow_date} to {self.expected_return_date}."
+        )
+
+    def clean(self):
+        if not self.borrow_date:
+            self.borrow_date = date.today()
+
+        if self.expected_return_date and self.expected_return_date < self.borrow_date:
+            raise ValidationError(
+                "Expected return date cannot be set up earlier than the borrow date."
+            )
+
+        if self.actual_return_date and self.actual_return_date < self.borrow_date:
+            raise ValidationError(
+                "Expected return date cannot be earlier than the borrow date."
+            )
+
+        if self.book.inventory == 0:
+            raise ValidationError(
+                f"The book '{self.book.title}' is not available for borrowing."
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
