83d4869b38f022380fd520d868ec8c6cfcb8ed25
olena4reunited <olenaduringintermission@gmail.com>
2024-10-19 15:55:53 +0300

    fix: update github workflow & telegram_bot.py to run in test mode

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,20 +8,35 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 
+
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 CHAT_ID = getenv("CHAT_ID")
+TEST_MODE = getenv("TEST_MODE", "False").lower() == "true"
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
 
 if not TOKEN or not CHAT_ID:
     logging.error("BOT_TOKEN or CHAT_ID not provided")
     sys.exit("Please check .env file.")
 
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+class MockBot:
+    async def send_message(self, chat_id, text):
+        logger.info(f"Mock bot sending message to {chat_id}: {text}")
+
+
+if TEST_MODE:
+    logger.info("Running in test mode with mock bot")
+    bot = MockBot()
+else:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
 dp = Dispatcher()
 router = Router()
 
 dp.include_router(router)
-
 
 async def send_borrowing_notification(message: str) -> None:
     """
@@ -37,9 +52,12 @@ async def main() -> None:
     """
     Main func to run dispatcher
     """
+    if TEST_MODE:
+        logger.info("Test mode: Skipping dispatcher polling")
+
     await dp.start_polling(bot)
 
 
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
